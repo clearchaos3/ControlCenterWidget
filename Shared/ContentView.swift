d2215652d093fc9ea15ab6c3b50fcc151fc4e9f2
clearchaos3 <clearchaos3@gmail.com>
2020-06-30 23:26:31 -0500

    updated music player widget and added album art

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+struct Song {
+    var title: String
+    var artist: String
+    var image: String
+}
+
+let songs = [
+    Song(title: "I'd Do Anything", artist: "Simple Plan", image: "NoPads"),
+    Song(title: "All Star", artist: "Smash Mouth", image: "AstroLounge"),
+    Song(title: "Slow Jamz", artist: "Kanye West", image: "CollegeDropout")
+]
+
+
+
 struct ContentView: View {
     @State var spacing: CGFloat = 12
     @State var isExpanded = false
@@ -15,72 +29,37 @@ struct ContentView: View {
     @State var wifiEnabled = true
     @State var notifsEnabled = true
     @State var playMusic = false
+    @State var currentSong = 0
+    @State var count = (songs.count - 1)
+    @State var airplay = false
+    
+    func changeSong(_ direction: String) -> Int {
+        if direction == "previous" {
+            currentSong -= 1
+        } else {
+            currentSong += 1
+        }
+        if currentSong < 0 {
+            currentSong = count
+        }
+        if currentSong > count {
+            currentSong = 0
+        }
+        
+        return currentSong
+        
+    }
+    
     
     
     var body: some View {
-        HStack {
-            Spacer()
-            //Controls
+        VStack {
             VStack(spacing: self.spacing) {
                 HStack(spacing: self.spacing) {
-                    Button(action: {self.airplaneEnabled.toggle() } ) {
-                        VStack {
-                            Ellipse()
-                                .foregroundColor(Color(self.airplaneEnabled ? UIColor.systemOrange : UIColor.tertiarySystemGroupedBackground))
-                                .overlay(Image(systemName:"airplane"))
-                                .frame(width: 52, height: 52)
-                            if self.isExpanded {
-                                Text("Airplane")
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                            }
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    Button(action: {self.cellularEnabled.toggle() } ) {
-                        VStack {
-                            Ellipse()
-                                .foregroundColor(Color(self.cellularEnabled ? UIColor.systemGreen : UIColor.tertiarySystemGroupedBackground))
-                                .overlay(Image(systemName:"antenna.radiowaves.left.and.right"))
-                                .frame(width: 52, height: 52)
-                            if self.isExpanded {
-                                Text("Cellular")
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                            }
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                }
-                HStack(spacing: self.spacing) {
-                    Button(action: {self.wifiEnabled.toggle() } ) {
-                        VStack {
-                            Ellipse()
-                                .foregroundColor(Color(self.wifiEnabled ? UIColor.systemBlue : UIColor.tertiarySystemGroupedBackground))
-                                .overlay(Image(systemName: self.wifiEnabled ? "wifi" : "wifi.slash"))
-                                .frame(width: 52, height: 52)
-                            if self.isExpanded {
-                                Text("Wifi")
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                            }
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    Button(action: {self.notifsEnabled.toggle() } ) {
-                        VStack {
-                            Ellipse()
-                                .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
-                                .overlay(Image(systemName: self.notifsEnabled ? "bell.fill" : "bell.slash.fill"))
-                                .frame(width: 52, height: 52)
-                            if self.isExpanded {
-                                Text("Notifs")
-                                    .font(.caption)
-                                    .fontWeight(.semibold)
-                            }
-                        }
-                    }
-                    .buttonStyle(PlainButtonStyle())
+                    Image(songs[currentSong].image)
+                        .resizable()
+                        .frame(width: 290, height: 290)
+                        .cornerRadius(12.0)
                 }
             }
             .padding()
@@ -99,42 +78,139 @@ struct ContentView: View {
                     }
                 }
             }
-            Spacer()
-            //Music
-            VStack(spacing: self.spacing) {
-                
-                VStack(spacing: 8) {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "airplayaudio")
-                            .opacity(0.5)
-                    }
-                    Text("I'd Do Anything")
-                        .font(.subheadline).bold().padding(.bottom, -5.0)
-                    Text("Simple Plan")
-                        .font(.callout)
-                        .foregroundColor(Color(UIColor.systemGray))
-                        .padding(0)
-                    HStack{
-                        Image(systemName: "backward.fill")
-                            .padding(2)
-                        Button(action: {self.playMusic.toggle() } ) {
-                            Image(systemName: self.playMusic ? "pause.fill" : "play.fill")
-                                .resizable()
-                                .frame(width:15, height: 20)
-                                .padding()
+            HStack {
+                Spacer()
+                //Controls
+                VStack(spacing: self.spacing) {
+                    HStack(spacing: self.spacing) {
+                        Button(action: {self.airplaneEnabled.toggle() } ) {
+                            VStack {
+                                Ellipse()
+                                    .foregroundColor(Color(self.airplaneEnabled ? UIColor.systemOrange : UIColor.tertiarySystemGroupedBackground))
+                                    .overlay(Image(systemName:"airplane"))
+                                    .frame(width: 52, height: 52)
+                                if self.isExpanded {
+                                    Text("Airplane")
+                                        .font(.caption)
+                                        .fontWeight(.semibold)
+                                }
+                            }
                         }
                         .buttonStyle(PlainButtonStyle())
-                        Image(systemName: "forward.fill")
-                            .padding(2)
+                        Button(action: {self.cellularEnabled.toggle() } ) {
+                            VStack {
+                                Ellipse()
+                                    .foregroundColor(Color(self.cellularEnabled ? UIColor.systemGreen : UIColor.tertiarySystemGroupedBackground))
+                                    .overlay(Image(systemName:"antenna.radiowaves.left.and.right"))
+                                    .frame(width: 52, height: 52)
+                                if self.isExpanded {
+                                    Text("Cellular")
+                                        .font(.caption)
+                                        .fontWeight(.semibold)
+                                }
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    HStack(spacing: self.spacing) {
+                        Button(action: {self.wifiEnabled.toggle() } ) {
+                            VStack {
+                                Ellipse()
+                                    .foregroundColor(Color(self.wifiEnabled ? UIColor.systemBlue : UIColor.tertiarySystemGroupedBackground))
+                                    .overlay(Image(systemName: self.wifiEnabled ? "wifi" : "wifi.slash"))
+                                    .frame(width: 52, height: 52)
+                                if self.isExpanded {
+                                    Text("Wifi")
+                                        .font(.caption)
+                                        .fontWeight(.semibold)
+                                }
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                        Button(action: {self.notifsEnabled.toggle() } ) {
+                            VStack {
+                                Ellipse()
+                                    .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
+                                    .overlay(Image(systemName: self.notifsEnabled ? "bell.fill" : "bell.slash.fill"))
+                                    .frame(width: 52, height: 52)
+                                if self.isExpanded {
+                                    Text("Notifs")
+                                        .font(.caption)
+                                        .fontWeight(.semibold)
+                                }
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
                     }
                 }
+                .padding()
+                .background(Color(UIColor.secondarySystemBackground))
+                .cornerRadius(24.0)
+                .onTapGesture {
+                    withAnimation(
+                        .easeInOut(duration: 0.25)
+                    ){
+                        self.isExpanded.toggle()
+                        
+                        if self.isExpanded {
+                            self.spacing = 24
+                        } else {
+                            self.spacing = 12
+                        }
+                    }
+                }
+                Spacer()
+                
+                //Music
+                VStack(spacing: self.spacing) {
+                    
+                    
+                    VStack(spacing: 8) {
+                        
+                        
+                        HStack {
+                            Spacer()
+                            Button(action: {self.airplay.toggle() } ) {
+                                Image(systemName: "airplayaudio")
+                                    .foregroundColor(Color(airplay ? UIColor.systemBlue : UIColor.systemGray))
+                                    .opacity(airplay ? 1 : 0.5)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                        Text(songs[currentSong].title)
+                            .font(.subheadline).bold().padding(.bottom, -5.0)
+                        Text(songs[currentSong].artist)
+                            .font(.callout)
+                            .foregroundColor(Color(UIColor.systemGray))
+                            .padding(0)
+                        HStack{
+                            Button(action: {_ = self.changeSong("previous") } ) {
+                                Image(systemName: "backward.fill")
+                            }
+                                .padding(2)
+                            .buttonStyle(PlainButtonStyle())
+                            Button(action: {self.playMusic.toggle() } ) {
+                                Image(systemName: self.playMusic ? "pause.fill" : "play.fill")
+                                    .resizable()
+                                    .frame(width:15, height: 20)
+                                    .padding()
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            Button(action: {_ = self.changeSong("next") } ) {
+                                Image(systemName: "forward.fill")
+                            }
+                                .padding(2)
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                    }
+                }
+                .padding(12.0)
+                .frame(width: 150.0, height: 150.0)
+                .background(Color(UIColor.secondarySystemBackground))
+                .cornerRadius(24.0)
+                
+                Spacer()
             }
-            .padding(12.0)
-            .frame(width: 150.0, height: 150.0)
-            .background(Color(UIColor.secondarySystemBackground))
-            .cornerRadius(24.0)
-            
             Spacer()
         }
     }
